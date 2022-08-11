afa67c4e255e8affe952694c3d6ed2a242417afd
zhangsenhao <15138328869@163.com>
2022-08-11 10:46:16 +0800

    0811上午提交

--- 0810.js
+++ 0810.js
@@ -19,5 +19,15 @@ var name='小明';
 var age=20;
 console.log(`你好,${name},你今年${age}岁了`);//反引号的使用
 
+var s='HELLO,world!';
+s.length;//获取字符串的数量，length：长、长度。
+s.toUpperCase();//把字符串全部变为大写，toUpperCase：转换为大写
+s.toLowerCase();//将字符串全部变为小写，tolowercase：转换为小写
+console.log(s);//输出s的内容到控制台，console：控制台。
+alert(s);//弹窗输出s，alert：提示，提醒。
+s.indexOf('world')//返回6，查找指定字符串的位置
+s.indexOf('a')//没有指定字符串，返回-1
+s.substring(0,5);//返回：HELLO,从索引0开始取值到5（不包含5）
+s.substring(6);//从索引6开始取值到结束，返回：world
 
 
